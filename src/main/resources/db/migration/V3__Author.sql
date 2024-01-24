e4b99836489242e9e1723f7afcc096539f691290
Viktor Martynov <2013alfabravo@gmail.com>
2024-01-25 01:05:45 +0400

    create gin index on Author name

--- src/main/resources/db/migration/V3__Author.sql
+++ src/main/resources/db/migration/V3__Author.sql
@@ -8,3 +8,6 @@ create table author
 alter table budget add column author_id int;
 
 alter table budget add foreign key (author_id) references author(id);
+
+create extension if not exists pg_trgm;
+create index idx_full_name_gin on author using gin (full_name gin_trgm_ops);
